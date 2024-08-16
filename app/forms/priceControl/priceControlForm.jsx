3a0bbab86af9270a3b4671fc598d5a13a5c4fb24
Mubassir Hayat <mubassirhayat@gmail.com>
2024-08-16 12:42:15 +0500

    feat: date picker added to the form.

--- app/forms/priceControl/priceControlForm.jsx
+++ app/forms/priceControl/priceControlForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, FlatList, Alert } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, FlatList, Alert, Pressable, Platform } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocalSearchParams, router } from 'expo-router';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
@@ -7,6 +7,7 @@ import {dbDate } from '@/utils/formatDate';
 import LocationDropdown from '@/components/locationDropdown';
 import useHeaderTitle from '@/hooks/useHeaderTitle';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from "moment";
 
 
 const PriceControlForm = () => {
@@ -17,15 +18,34 @@ const PriceControlForm = () => {
   const offlineActivities = useSelector((state) => state.law.offlineActivities);
   const activity = activities?.find((act) => act.start === actStart) || {};
   const [modalVisible, setModalVisible] = useState(false);
-  const [mediaType, setMediaType] = useState('');
-
-  const [title, setTitle] = useState(activity?.title || '');
-  const [type, setType] = useState(activity?.type || '');
-  const [start, setStart] = useState(activity?.start || dbDate());
-  const [end, setEnd] = useState(activity?.end || '');
+  const [date, setDate] = useState(new Date());
+  const [dateOfList, setDateOfList] = useState('');
+  const [showPicker, setShowPicker] = useState(false);
   const [location, setLocation] = useState(activity?.location || '');
   const [description, setDescription] = useState(activity?.description || '');
-  const [attachments, setAttachments] = useState(activity?.attachments || []);
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker);
+  }
+
+  const onChange = ({ type }, selectedDate) => {
+    if (type == 'set') {
+      const currentDate = selectedDate;
+      setDate(currentDate);
+
+      if (Platform.OS === 'android') {
+        toggleDatePicker();
+        setDateOfList(currentDate.toDateString());
+      }
+    } else {
+      toggleDatePicker();
+    }
+  }
+
+  const confirmDateOfList = () => {
+    setDateOfList(currentDate.toDateString());
+    toggleDatePicker();
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -38,8 +58,64 @@ const PriceControlForm = () => {
         onValueChange={setLocation}
       />
 
-      <Text style={styles.label}>Date and Time</Text>
-      <RNDateTimePicker display="spinner" />
+      <Text style={styles.label}>Date</Text>
+      {showPicker && (
+        <DateTimePicker
+          value={date}
+          mode={'date'}
+          display='spinner'
+          onChange={onChange}
+          style={styles.datePicker}
+        />
+      )}
+      {showPicker && Platform.OS === "ios" && (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around"
+          }}
+        >
+          <TouchableOpacity style={[
+            styles.button,
+            styles.pickerButton,
+            {backgroundColor: "#11182711"}
+          ]}
+            onPress={toggleDatePicker}
+          >
+            <Text 
+              style={[
+                styles.buttonText,
+                {color: "#075985"}
+              ]}
+              >Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[
+            styles.button,
+            styles.pickerButton,
+          ]}
+            onPress={confirmDateOfList}
+          >
+            <Text 
+              style={[
+                styles.buttonText,
+              ]}
+              >Confirm</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {!showPicker && (
+        <Pressable onPress={toggleDatePicker}>
+          <TextInput
+            style={styles.input}
+            placeholder='Select date and time'
+            value={dateOfList}
+            onChangeText={setDateOfList}
+            placeholderTextColor='#11182744'
+            editable={false}
+            onPressIn={toggleDatePicker}
+          />
+        </Pressable>
+      )}
       
       <Text style={styles.label}>Description</Text>
       <TextInput
@@ -112,6 +188,27 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
   },
+  datePicker: {
+    width: 120,
+    marginTop: -10,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#fff',
+  },
+  button: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#075985',
+  },
+  pickerButton: {
+    paddingHorizontal: 20,
+  }
 });
 
 export default PriceControlForm;
